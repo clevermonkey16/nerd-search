2b56df3bc2341bdffe6ab85d29ef236490a224a3
Frank Wang <frankningwang@gmail.com>
2024-02-19 15:17:14 -0800

    started frontend

---
+++ frontend/src/components/JobBoard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import JobOverview from "./JobOverview";
+
+function JobBoard() {
+  return <JobOverview />;
+}
+
+export default JobBoard;
---
+++ frontend/src/components/JobOverview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function JobOverview() {
+  return (
+    <>
+      <div>JobOverview</div>
+      <div>JobOverview</div>
+    </>
+  );
+}
+
+export default JobOverview;

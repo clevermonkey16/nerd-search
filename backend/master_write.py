bc522a7f8659d565e2a91626b836f975bcce7c17
clevermonkey16 <clevermonkey16@gmail.com>
2024-04-07 21:07:43 -0700

    added scrape function to master_write.py

--- backend/master_write.py
+++ backend/master_write.py
@@ -7,3 +7,19 @@
 # 5.5 Possibly have to nuke invalid postings in other databases 
 # 6. Close the database connection
 
+from Webscrapers import workday
+
+def scrape(type, link):
+    if type == "workday":
+        workday.scrape(link)
+    elif type == "eightfold":
+        eightfold.scrape(link)
+    elif type == "lever":
+        lever.scrape(link)
+    else:
+        print("bruh it no correct")
+        pass
+        
+if __name__ == "__main__":
+    scrape("workday", "https://nvidia.wd5.myworkdayjobs.com/NVIDIAExternalCareerSite?q=intern&locationHierarchy1=2fcb99c455831013ea52fb338f2932d8")
+

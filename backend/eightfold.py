ad65b6bbab564173647cccdd1a5dd96bea304a91
Laurence <laurenceliao123@gmail.com>
2024-04-15 22:49:50 -0700

    fsfs

--- backend/eightfold.py
+++ backend/eightfold.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 def scrape(link):
     # Set Path for to ChromeDriver
@@ -15,42 +16,36 @@ def scrape(link):
     driver.get(website)
 
     time.sleep(5)
-    #test
+
     #Titles and location are a lists
     try:
         removeBlocker = driver.find_element(By.XPATH, '//button[@class="onetrust-close-btn-handler onetrust-close-btn-ui banner-close-button ot-close-icon"]')
         removeBlocker.click()
     except:
-        print("hello")
+        print("ignore")
+
     showMorePositions = driver.find_element(By.XPATH, '//button[@class="btn btn-sm btn-secondary show-more-positions"]')
-    driver.execute_script("arguments[0].scrollIntoView();", showMorePositions)
+    
     time.sleep(5)
     
     actions = ActionChains(driver)
     actions.move_to_element(showMorePositions).perform()
-    actions.move_by_offset(0, -(driver.get_window_size()['height'] / 2))
 
+    for _ in range(10):
+        driver.find_element(By.XPATH, '//button[@class="btn btn-sm btn-secondary show-more-positions"]').send_keys(Keys.ARROW_DOWN)
     
     time.sleep(5)
     showMorePositions.click()
     titles = driver.find_elements(By.XPATH, '//*[contains(@data-test-id, "position-card")]')
-    #location = driver.find_elements(By.XPATH, '//p[@class="field-label"]')
 
     for j in range(len(titles)):
         i = titles[j]
         #location_info = location[j].text
         #link = titles[j].get_attribute("href")
         i.click()
-<<<<<<< HEAD
         time.sleep(3)
         job_link = driver.current_url
         location_info = driver.find_element(By.XPATH, '//*[@class="position-location"]').text
-=======
-
-        time.sleep(5)
-        job_link = driver.current_url
-
->>>>>>> 7b6ae08cd9bf913efaa1e868bc80782f55ed62df
         job_title_info = driver.find_element(By.XPATH, '//*[@class="position-title"]').text
         job_info = driver.find_element(By.XPATH, '//div[@class="position-job-description"]').text
 
@@ -62,10 +57,6 @@ def scrape(link):
 
         values = (job_title_info, location_info, job_info, date_posted, job_link, 1)
         SQL_data.insert(values)
-<<<<<<< HEAD
-=======
-        time.sleep(3)
->>>>>>> 7b6ae08cd9bf913efaa1e868bc80782f55ed62df
 
 
         

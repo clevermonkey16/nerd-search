86d501096c762e6b1ab51c8e10284917e5b95c0d
Laurence <laurenceliao123@gmail.com>
2024-04-16 21:08:49 -0700

    more changes

--- backend/master_write.py
+++ backend/master_write.py
@@ -8,6 +8,7 @@
 # 6. Close the database connection
 
 import csv
+import os
 
 import workday
 import greenhouse
@@ -37,7 +38,6 @@ if __name__ == "__main__":
     SQL_data.allInvalid()
 
     # 3. Read company links from companies.csv
-    
     with open('companies.csv', 'r') as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:

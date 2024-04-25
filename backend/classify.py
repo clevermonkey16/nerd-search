fda3e1e0b86b831a8ad8af5bbffc4864a60b25fe
clevermonkey16 <clevermonkey16@gmail.com>
2024-04-25 16:24:01 -0700

    hqwaig4oh

---
+++ backend/classify.py
@@ -0,0 +1,19 @@
+import writedata
+import classify
+
+def removeNonTech():
+    # 1. Connect to the database
+
+    # Iterate over rows of database
+
+    # for each row
+    # get job description, job title
+    # if category is na, run black box classifier on info
+    # if classifier returns false, make row bit invaild
+    # else run black box classifier to get category, update category 
+
+    # nuke table of invalid jobs
+
+    # close database connection
+
+    # NOTE: Keyword search goes at the top of the blackbox classifier

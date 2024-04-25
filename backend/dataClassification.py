f504bdcf94428cc89f4751e84ce1575150786294
noah tang <41347722+NoahTang@users.noreply.github.com>
2024-04-25 16:03:12 -0700

    the black box is black boxing

---
+++ backend/dataClassification.py
@@ -0,0 +1,37 @@
+#connect to database
+#
+#preprocess text using nltk
+#check for tech/nontech and set nontech jobs to invalid (within a threshold)
+#nuke invalid
+# a. keyword search titles for the list of keywords that would determine you know what
+# b. run the model on everything else, insert proper labels
+#disconnect from the database
+
+from writedata import SQLWriter
+
+import pandas as pd
+import numpy as np
+import nltk
+import json
+import string
+import re
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+#nltk.download('all', force=True)
+
+def kWordSearch(title):
+    keywords = {'backend': ["software engineer", "software developer",
+                             "java developer", "c++ developer", 
+                             "javascript developer", "back-end developer",
+                             "backend developer"],
+                'cloud': ["cloud", "azure"],
+                'devops': ["devops"], 
+                'fullstack': ["full stack", "fullstack"],
+                'security': ["security", "cyber"], 
+                'ui_ux': ["frontend", "ui", "ux"]}
+    
+
+DATABASE = "backend/jobs.db"
+

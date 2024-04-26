6b96e17f06fa7a92e201d9be2245e27120352238
Frank Wang <frankningwang@gmail.com>
2024-04-26 02:43:06 -0700

    wheee

--- backend/backend.py
+++ backend/backend.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from writedata import SQLWriter
 import sqlite3
 import os
+from flask import send_file
 
 app = Flask(__name__)
 CORS(app)
@@ -50,6 +51,22 @@ def list_files():
     ]
     # Return the list of files as JSON data
     return jsonify(files)
+
+
+@app.route("/file/<filename>")
+def get_file_content(filename):
+    # Get the path to the current directory
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    # Construct the full path to the requested file
+    file_path = os.path.join(current_directory, filename)
+
+    # Check if the file exists
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        # Return the file contents as a response
+        return send_file(file_path)
+    else:
+        # Return a 404 Not Found error if the file doesn't exist
+        return "File not found", 404
 
 
 @app.route("/jobs", methods=["GET"])

ced36ab4fad69db2558a9788239c1ce6e872afa1
clevermonkey16 <clevermonkey16@gmail.com>
2024-04-26 05:02:23 -0700

    classify script ready

--- backend/classifier.py
+++ backend/classifier.py
@@ -1,5 +1,5 @@
 import writedata
-#from dataClassification import data_classify as black_box
+from dataClassification import data_classify as black_box
 
 def classify():
     # 1. Connect to the database
@@ -21,15 +21,30 @@ def classify():
         #valid = jobs[i][6]
         category = jobs[i][7]
 
+
         # determining tech vs non-tech
         is_this_thing_valid_or_not = black_box.isTech(title)
 
         # If classifier returns false, make row bit invalid
         if not is_this_thing_valid_or_not:
             SQL_data.updateValid(link, 0)
+        
+        # determine if it's intern or not
+        bad_words = ["internal", "international"]
+        for word in bad_words:
+            if word in title.lower():
+                SQL_data.updateValid(link, 0)
+                is_this_thing_valid_or_not = False
+                break
+
+        keyWord = black_box.kWordSearch(title.lower()) 
+
+        if keyWord != "no_kClass":
+            SQL_data.updateCategory(link, keyWord)
+            continue #maybe we should remove this later on - noah
 
         if is_this_thing_valid_or_not and category == "na":
-            # Run black box classifier on info
+            # Run black box classifier on infos
             cat = black_box.classify(description)
             SQL_data.updateCategory(link, cat)
 
